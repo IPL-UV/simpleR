39d360f0a071e14d7600b2c21b7828c619b9d157
gus <gus@cf90e129-9db2-4be2-879b-6cf61410cf68>
2015-09-24 08:43:57 +0000

    git-svn-id: https://harpo.uv.es/repos/ipl/Software@575 cf90e129-9db2-4be2-879b-6cf61410cf68

---
+++ methods_list.m
@@ -0,0 +1,42 @@
+% ------------------------------
+% METHODS
+% ------------------------------
+
+% LINEAR MODELS
+%    * Regularized Least squares Linear regression (RLR) 
+%    * Least Absolute Shrinkage and Selection Operator (LASSO).
+%    * Elastic Net (ELASTICNET).
+
+% SPLINES and POLYNOMIALS
+% ---* Adaptive Regression Splines (ARES)
+% ---* Locally Weighted Polynomials (LWP)
+
+% NEIGHBORS
+% ---* K-nearest neighbors regression (KNNR)
+
+%   TREE MODELS
+%    * Decision trees (TREE)
+%    * Bagging trees (BAGTREE)
+%    * Boosting trees (BOOST)
+%    * Random forests (RF1)
+%    * Boosting random trees (RF2)
+
+% NEURAL NETWORS
+%    * Neural networks (NN)
+%    * Extreme Learning Machines (ELM)
+
+% KERNEL METHODS
+%    * Support Vector Regression (SVR)
+%    * Kernel Ridge Regression (KRR), aka Least Squares SVM
+%    * Relevance Vector Machine (RVM)
+%  ---  * Kernel signal to noise regression (KSNR)
+%  ---  * Structured KRR (SKRR)
+%  ---  * Random Kitchen Sinks Regression (RKS)
+%
+% % GAUSSIAN PROCESSES
+%    * Gaussian Process Regression (GPR)
+%    * Variational Heteroscedastic Gaussian Process Regression (VHGPR)
+%  ---  * Warped Gaussian Processes (WGPR)
+%  ---  * Sparse spectrum Gaussian Process Regression (SSGPR)
+%  ---  * Twin Gaussian Processes (TGP)
+
