91b37ba7afd79633acb9848ddfaea7ef37c7bdde
gus <gus@cf90e129-9db2-4be2-879b-6cf61410cf68>
2015-09-24 08:51:38 +0000

    git-svn-id: https://harpo.uv.es/repos/ipl/Software@576 cf90e129-9db2-4be2-879b-6cf61410cf68

--- simpleRegression.m
+++ simpleRegression.m
@@ -186,8 +186,8 @@ set(0,'DefaultAxesFontName',fontname,'DefaultAxesFontSize',fontsize,'DefaultAxes
     'DefaultLineLineWidth',3,'DefaultLineMarkerSize',10,'DefaultLineColor',[0 0 0]);
 
 % Paths
-addpath('./vhgpr')                  % VHGPR [Lázaro-Gredilla, 2011]
 addpath('./standard')               % Standard statistical regression: LR, LASSO, TREES, SVR, KRR, GPR
+addpath('./vhgpr')                  % VHGPR [Lázaro-Gredilla, 2011]
 
 clear;clc;close all;
 
@@ -218,195 +218,57 @@ Ytrain  = Ytrain - my;
 
 % METHODS = {'RLR' 'LASSO' 'ELASTICNET' 'TREE' 'BAGTREE' 'BOOST' 'NN' 'ELM' 'SVR' 'KRR' 'RVM' 'GPR' 'VHGPR'}
 % METHODS = {'RLR' 'TREE' 'BAGTREE' 'BOOST' 'NN' 'ELM' 'SVR' 'KRR' 'RVM' 'GPR' 'VHGPR'}
-METHODS = {'RLR' 'LASSO' 'ELASTICNET'} % LINEAR
+% METHODS = {'RLR' 'LASSO' 'ENET'} % LINEAR
 % METHODS = {'TREE' 'BAGTREE' 'BOOST'} % TREES
 % METHODS = {'NN' 'ELM'} % NEURAL NETS
 % METHODS = {'SVR' 'KRR' 'RVM' 'GPR' 'VHGPR'} % KERNELS
 % METHODS = {'ELM'}
 
-% Counter for the method used
-m=0;
+METHODS = {'RLR' 'LASSO' 'ENET' }%'ARES' 'LWP'}
 
-%% (1) REGULARIZED LEAST SQUARES LINEAR REGRESSION
-if sum(strcmpi(METHODS,'RLR'))
-    m=m+1;
+%% TRAIN ALL MODELS
+numModels = numel(METHODS);
+
+for m=1:numModels
+    fprintf(['Training ' METHODS{m} '... \n'])
+    
     t=cputime;
-    fprintf('Training RLR ... \n')
-    model_RLR      = trainRLR(Xtrain,Ytrain);
-    Yp             = testRLR(model_RLR,Xtest);
+    eval(['model = train' METHODS{m} '(Xtrain,Ytrain);']); % Train the model
+    eval(['Yp = test' METHODS{m} '(model,Xtest);']); % Test the model
     Ypred(:,m)     = Yp + my;
-    resultsRLR     = assessment(Ypred(:,m),Ytest,'regress');
-    tempsLR        = cputime-t;
+%     %     Ypred(:,m)     = (Yp+0.5)*(Ma-mi) + mi;
+%     if phys==1
+%         results(m)     = assessment(10.^Ypred(:,m),10.^(Ytest),'regress');
+%     else
+        results(m)     = assessment(Ypred(:,m),Ytest,'regress');
+%     end
+    %     results(m)     = assessment(log10(Ypred(:,m)),log10(Ytest),'regress')
+    %     results(m)     = assessment(Ypred(:,m).^(1/4),Ytest.^(1/4),'regress')
+    %     results(m)     = assessment(Ypred(:,m).^(1/2),Ytest.^(1/2),'regress')
+    
+    CPUTIMES(m) = cputime-t;
 end
 
-%% (2) LASSO
-if sum(strcmpi(METHODS,'LASSO'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training LASSO ... \n')
-    model_LASSO     = trainLASSO(Xtrain,Ytrain);
-    Yp              = testLASSO(model_LASSO,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsLASSO    = assessment(Ypred(:,m),Ytest,'regress');
-    tempsLASSO      = cputime-t;
-end
 
-%% (3) ELASTIC NET
-if sum(strcmpi(METHODS,'ELASTICNET'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training Elastic Net ... \n')
-    model_ElasticNet = trainElasticNet(Xtrain,Ytrain);
-    Yp               = testElasticNet(model_ElasticNet,Xtest);
-    Ypred(:,m)       = Yp + my;
-    resultsENET      = assessment(Ypred(:,m),Ytest,'regress');
-    tempsENET        = cputime-t;
-end
-
-%% (4) TREES
-if sum(strcmpi(METHODS,'TREE'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training Trees ... \n')
-    model_TREE      = trainTREE(Xtrain,Ytrain);
-    Yp              = testTREE(model_TREE,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsTREE     = assessment(Ypred(:,m),Ytest,'regress');
-    tempsTREE       = cputime-t;
-end
-
-%% (5) Bagging Trees
-if sum(strcmpi(METHODS,'BAGTREE'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training Bagging Trees ... \n')
-    model_BAGTREE   = trainBAGTREE(Xtrain,Ytrain);
-    Yp              = testTREEBAG(model_BAGTREE,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsBAGTREE  = assessment(Ypred(:,m),Ytest,'regress');
-    tempsBAGTREE    = cputime-t;
-end
-
-%% (6) Boosting trees
-if sum(strcmpi(METHODS,'BOOST'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training Boosting Trees ... \n')
-    model_BOOST     = trainBOOST(Xtrain,Ytrain);
-    Yp              = testBOOST(model_BOOST,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsBOOST    = assessment(Ypred(:,m),Ytest,'regress');
-    tempsBOOST      = cputime-t;
-end
-
-%% (7) NN
-if sum(strcmpi(METHODS,'NN'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training NN ... \n')
-    warning off
-    model_NN        = trainNN(Xtrain,Ytrain);
-    Yp              = testNN(model_NN,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsNN       = assessment(Ypred(:,m),Ytest,'regress');
-    tempsNN         = cputime-t;
-end
-
-%% (8) Extreme Learning Machine (ELM)
-if sum(strcmpi(METHODS,'ELM'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training ELM ... \n')
-    model_ELM       = trainELM(Xtrain,Ytrain);
-    Yp              = testELM(model_ELM,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsELM      = assessment(Ypred(:,m),Ytest,'regress');
-    tempsELM        = cputime-t;
-end
-
-%% (9) SVR
-if sum(strcmpi(METHODS,'SVR'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training SVR ... \n')
-    model_SVR      = trainSVR(Xtrain,Ytrain);
-    Yp             = testSVR(model_SVR, Xtest);
-    Ypred(:,m)     = Yp + my;
-    resultsSVR     = assessment(Ypred(:,m),Ytest,'regress');
-    tempsSVR       = cputime-t;
-end
-
-%% (10) KRR (aka Least Squares SVM)
-if sum(strcmpi(METHODS,'KRR'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training KRR ... \n')
-    model_KRR       = trainKRR(Xtrain,Ytrain);
-    Yp              = testKRR(model_KRR,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsKRR      = assessment(Ypred(:,m),Ytest,'regress');
-    tempsKRR        = cputime-t;
-end
-
-%% (11) RELEVANCE VECTOR MACHINE
-if sum(strcmpi(METHODS,'RVM'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training RVM ... \n')
-    model_RVM       = trainRVM(Xtrain,Ytrain);
-    Yp              = testRVM(model_RVM,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsRVM      = assessment(Ypred(:,m),Ytest,'regress');
-    tempsRVM        = cputime-t;
-end
-
-%% (12) GPR
-if sum(strcmpi(METHODS,'GPR'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training GPR ... \n')
-    model_GP        = trainGPR(Xtrain,Ytrain);
-    [Yp S2]         = testGPR(model_GP,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsGP       = assessment(Ypred(:,m),Ytest,'regress');
-    tempsGP         = cputime-t;
-end
-
-%% (13) VHGPR
-if sum(strcmpi(METHODS,'VHGPR'))
-    m=m+1;
-    t=cputime;
-    fprintf('Training VHGPR ... \n')
-    model_VHGP      = trainVHGPR(Xtrain,Ytrain);
-    [Yp S2]         = testVHGPR(model_VHGP,Xtest);
-    Ypred(:,m)      = Yp + my;
-    resultsVHGP     = assessment(Ypred(:,m),Ytest,'regress');
-    tempsVHGP       = cputime-t;
-end
-
-%% NUMERICAL RESULTS
-clc
+%% NUMERICAL COMPARISON
+% clc
 fprintf('----------------------------------------------------------------------------------- \n')
-fprintf('METHOD             & ME    & RMSE  & MAE   & R \\\\ \n')
+fprintf('METHOD\t & ME\t & RMSE\t & MAE\t & R \\\\ \n')
 fprintf('----------------------------------------------------------------------------------- \n')
-fprintf('RLR                & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsRLR.ME),resultsRLR.RMSE,resultsRLR.MAE,resultsRLR.R)
-fprintf('LASSO              & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsLASSO.ME),resultsLASSO.RMSE,resultsLASSO.MAE,resultsLASSO.R)
-fprintf('ELASTIC            & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsENET.ME),resultsENET.RMSE,resultsENET.MAE,resultsENET.R)
+for m=1:numModels
+    fprintf([METHODS{m} '\t & %3.3f\t & %3.3f\t & %3.3f\t & %3.3f \\\\ \n'],abs(results(m).ME),results(m).RMSE,results(m).MAE,results(m).R)
+end
 fprintf('----------------------------------------------------------------------------------- \n')
-fprintf('TREE               & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsTREE.ME),resultsTREE.RMSE,resultsTREE.MAE,resultsTREE.R)
-fprintf('BAGTREE            & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsBAGTREE.ME),resultsBAGTREE.RMSE,resultsBAGTREE.MAE,resultsBAGTREE.R)
-fprintf('BOOST              & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsBOOST.ME),resultsBOOST.RMSE,resultsBOOST.MAE,resultsBOOST.R)
-fprintf('----------------------------------------------------------------------------------- \n')
-fprintf('NN                 & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsNN.ME),resultsNN.RMSE,resultsNN.MAE,resultsNN.R)
-fprintf('ELM                & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsELM.ME),resultsELM.RMSE,resultsELM.MAE,resultsELM.R)
-fprintf('----------------------------------------------------------------------------------- \n')
-fprintf('SVR                & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsSVR.ME),resultsSVR.RMSE,resultsSVR.MAE,resultsSVR.R)
-fprintf('KRR                & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsKRR.ME),resultsKRR.RMSE,resultsKRR.MAE,resultsKRR.R)
-fprintf('----------------------------------------------------------------------------------- \n')
-fprintf('RVM                & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsRVM.ME),resultsRVM.RMSE,resultsRVM.MAE,resultsRVM.R)
-fprintf('GP                 & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsGP.ME),resultsGP.RMSE,resultsGP.MAE,resultsGP.R)
-fprintf('VHGPR              & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n',abs(resultsVHGP.ME),resultsVHGP.RMSE,resultsVHGP.MAE,resultsVHGP.R)
-fprintf('----------------------------------------------------------------------------------- \n')
+CPUTIMES
+[val idx] = min([results.RMSE]);
+disp(['The best method in RMSE terms is: ' METHODS{idx}])
+[val idx] = min(abs([results.ME]));
+disp(['The best method in ME terms is: ' METHODS{idx}])
+[val idx] = max([results.R]);
+disp(['The best method in correlation terms is: ' METHODS{idx}])
+
+break
+
 
 %% THE ERROR BOXPLOTS
 figure,
