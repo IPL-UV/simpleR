7ce2e404168417d2bdeb6252dced22272310b5de
jordi <jordi@cf90e129-9db2-4be2-879b-6cf61410cf68>
2016-11-02 15:21:17 +0000

    Added new trainSVMc for SVM classification, and related files. In regression toolbox, fixex trainTREE, testTREE and trainNN.
    git-svn-id: https://harpo.uv.es/repos/ipl/Software@658 cf90e129-9db2-4be2-879b-6cf61410cf68

--- standard/trainTREE.m
+++ standard/trainTREE.m
@@ -1,6 +1,11 @@
 function model_TREE = trainTREE(Xtrain,Ytrain)
 
-model_TREE = treefit(Xtrain,Ytrain,'method','regression');
-% Uncomment this if you want to perform xval pruning ...
-[c,s,nn,best] = treetest(model_TREE,'cross',Xtrain,Ytrain);
-model_TREE    = treeprune(model_TREE,'level',best);
+strver = version;
+if str2double(strver(1)) <= 7
+    model_TREE = treefit(Xtrain, Ytrain, 'method', 'regression');
+    % Uncomment this if you want to perform xval pruning ...
+    [~,~,~,best] = treetest(model_TREE, 'cross', Xtrain, Ytrain);
+    model_TREE   = treeprune(model_TREE, 'level', best);
+else
+    model_TREE = fitrtree(Xtrain,Ytrain);
+end
